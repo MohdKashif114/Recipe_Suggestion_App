65907ea68f4a048cbfac3115317efd23c7ccddef
MohdKashif114 <166534290+MohdKashif114@users.noreply.github.com>
2025-05-04 06:34:28 +0530

    fixed vercel error

--- ClientFolder/src/app/recipes/page.tsx
+++ ClientFolder/src/app/recipes/page.tsx
@@ -4,7 +4,8 @@ import Cards from "@/components/Cards";
 import SearchBar from "@/components/SearchBar";
 import NavBar from "@/components/NavBar";
 import { useAuth } from "@/authcontext/Authcontext";
-import { Hourglass,Grid } from 'react-loader-spinner'
+import { ClipLoader } from "react-spinners";
+
 
 
 
@@ -18,15 +19,7 @@ export default function Home() {
 
 
  
-  if (!auth.posts) return <div className="bg-[#EFE3C2] h-screen w-screen flex justify-center items-center"><Hourglass
-  visible={true}
-  height="80"
-  width="80"
-  ariaLabel="hourglass-loading"
-  wrapperStyle={{}}
-  wrapperClass=""
-  colors={['#123524', '#3E7B27']}
-  /></div>
+  if (!auth.posts) return <div className="bg-[#EFE3C2] h-screen w-screen flex justify-center items-center"><ClipLoader color="#123524" size={150} /></div>
   
   
 
@@ -40,16 +33,7 @@ export default function Home() {
       {
         auth.loading ? (
           <div className="flex flex-col justify-center items-center h-screen pb-96 gap-4">
-            <Grid
-              visible={true}
-              height="80"
-              width="80"
-              color="#4fa94d"
-              ariaLabel="grid-loading"
-              radius="12.5"
-              wrapperStyle={{}}
-              wrapperClass="grid-wrapper"
-            />
+            <ClipLoader color="#123524" size={50} />
             <p className="text-center text-sm text-[#123524] px-4">
               Press the Refresh button if some recipes fail to appear!
             </p>
